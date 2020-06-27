314fedf87f0b49434183fbc7227a22c583b6d7c3
Ashu Sharma <ashu7073657271@gmail.com>
2020-06-28 00:47:25 +0530

    Add some gradient colors

--- lib/configs/ThemeColors.dart
+++ lib/configs/ThemeColors.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 
 class ThemeColors {
-    ThemeColors._();
+  ThemeColors._();
 
-    static const Color bg = Color(0xFFEFF3F6);
-    static Color main = Colors.blue[900];
+  static const Color bg = Color(0xFFEFF3F6);
+  static Color main = Colors.blue[900];
 //    static const Color main = Color(0xFFFF27B0);
 //    static const Color main = Color(0xFFEA9110);
 //    static const Color main = Color(0xFFFAA221);
-    static const Color shadowDark = Color.fromRGBO(0, 0, 0, 0.15);
-    static const Color shadowLight = Colors.white;
-    static  Color shadowColored = Color.fromRGBO(main.red, main.green, main.blue, 0.5);
+  static const Color shadowDark = Color.fromRGBO(0, 0, 0, 0.15);
+  static const Color shadowLight = Colors.white;
+  static Color shadowColored =
+      Color.fromRGBO(main.red, main.green, main.blue, 0.5);
+
+  static List<Color> gradient = [Color(0xFF396afc), Color(0xFF2948ff)];
+  static List<Color> gradient1 = [Color(0xFF11998e), Color(0xFF38ef7d)];
+  static List<Color> gradient2 = [Color(0xFFff9966), Color(0xFFff5e62)];
+  static List<Color> gradient3 = [Color(0xFF00d2ff), Color(0xFF3a7bd5)];
+
+  // static List<dynamic> gradientsList = [
+  //   gradient,
+  //   gradient1,
+  //   gradient2,
+  //   gradient3
+  // ];
 }
