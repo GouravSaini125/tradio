6a75a94c39a859a6de0a9ade260633aec83a6310
Ashu Sharma <ashu7073657271@gmail.com>
2020-06-29 03:20:34 +0530

    Add slideup page transition

--- lib/anims/page_route.dart
+++ lib/anims/page_route.dart
@@ -1,19 +1,46 @@
 import 'package:flutter/material.dart';
 
 Route createRoute(Widget widget) {
-    return PageRouteBuilder(
-        pageBuilder: (context, animation, secondaryAnimation) => widget,
-        transitionsBuilder: (context, animation, secondaryAnimation, child) {
-            var begin = Offset(0.0, 1.0);
-            var end = Offset.zero;
-            var curve = Curves.ease;
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => widget,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = Offset(0.0, 1.0);
+      var end = Offset.zero;
+      var curve = Curves.ease;
 
-            var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
 
-            return SlideTransition(
-                position: animation.drive(tween),
-                child: child,
-            );
-        },
-    );
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
+}
+
+class SlideUpRoute extends PageRouteBuilder {
+  final Widget page;
+  SlideUpRoute({this.page})
+      : super(
+          pageBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+          ) =>
+              page,
+          transitionDuration: Duration(milliseconds: 700),
+          transitionsBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            Widget child,
+          ) =>
+              SlideTransition(
+            position: Tween<Offset>(
+              begin: const Offset(0, 1),
+              end: Offset.zero,
+            ).animate(animation),
+            child: child,
+          ),
+        );
 }
